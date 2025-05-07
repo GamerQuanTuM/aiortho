be25f3467ae3773cf0c6374b9797c1f7eeb72f55
GamerQuanTuM <shuvamsantra10@gmail.com>
2025-05-07 18:45:50 +0530

    feat: update Home component and add Cloudflare deployment configuration
    - Refactored Home component in page.tsx to display an image from Unsplash.
    - Added GitHub Actions workflow for deploying to Cloudflare Workers.
    - Created Open Next configuration file for Cloudflare.
    - Added wrangler configuration for Cloudflare deployment assets.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config:NextConfig = {
+  output: 'standalone',
+  outputFileTracingExcludes: {
+    '*': ['node_modules/**/*']
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "daggle.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default config;
